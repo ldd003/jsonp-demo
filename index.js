21bc1beef276245f70dba17b459603b3043e5cbb
ldd003 <873652231@qq.com>
2019-12-27 16:32:37 +0800

    for es6

--- index.js
+++ index.js
@@ -2,54 +2,59 @@
  *
  * @param {String} url
  * @param {Object|Function} opts
- * @param {Function} fn
+ * @param {Function} cb
  *
  * opts:
- *  -param(String) 参数 默认callback
- *  -prefix(String) 回调函数名字前缀 默认jp
- *  -name(String) 自定义回调函数名字
+ *  -param(String) 参数 默认'callback'
+ *  -name(String) 自定义回调函数名
+ *  -prefix(String) 回调函数名前缀 默认'jp'
  *  -timeout(Number) 超时 默认60000
  *
  */
-function jp(url, opts = {}, fn) {
-  if (typeof opts == 'function') {
-    fn = opts;
+function jp(url, opts, cb) {
+  if (typeof opts == "function") {
+    cb = opts;
     opts = {};
   }
+  if (!opts) opts = {};
 
   // opts
-  const param = opts.param || 'callback';
-  const prefix = opts.prefix || 'jp';
-  const name =
-    opts.name || prefix + String(Math.random() * 10).replace('.', '');
-  const timeout = opts.timeout != null ? opts.timeout : 60000;
+  let param = opts.param || "callback";
+  let prefix = opts.prefix || "jp";
+  let name = opts.name || prefix + String(Math.random() * 10).replace(".", "");
+  let timeout = opts.timeout != null ? opts.timeout : 60000;
 
   // clean ->script callback timer
   function clean() {
-    document.body.removeChild(script);
+    if (script) document.body.removeChild(script);
     window[name] = function() {};
-    clearTimeout(timer);
+    if (timer) clearTimeout(timer);
   }
 
   // timer
-  const timer = setTimeout(() => {
+  let timer = setTimeout(() => {
     clean();
-    if (fn) fn(new Error('Timeout'));
+    if (cb) cb(new Error("Timeout"));
   }, timeout);
 
   // callback
   window[name] = function(data) {
     clean();
-    if (fn) fn(null, data);
+    if (cb) cb(null, data);
   };
 
   // script
-  let src = url + (url.includes('?') ? '&' : '?') + param + '=' + name;
-  src = src.replace('?&', '?');
+  let src =
+    url +
+    (url.includes("?") ? "&" : "?") +
+    param +
+    "=" +
+    encodeURIComponent(name);
+  src = src.replace("?&", "?");
 
-  let script = document.createElement('script');
+  let script = document.createElement("script");
   script.src = src;
   document.body.appendChild(script);
 }
 
-module.exports = jp;
+export default jp;
